24db864ed8f273e62e485d5571afa60703928829
naimahglin <naimahglin@gmail.com>
2024-08-07 00:59:04 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import EmployeeForm from './Components/EmployeeForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Employee Form</h1>
+      <EmployeeForm />
+    </div>
+  );
+}
+
+export default App;
+
